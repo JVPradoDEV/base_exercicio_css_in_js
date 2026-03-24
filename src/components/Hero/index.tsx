f539105dd59f3a570203f59effa648bf1a78be0a
bonnie <joaovitordoprado15@hotmail.com>
2026-03-24 18:54:12 -0300

    Conversão de CSS para Styled Components

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,13 +1,14 @@
-import styles from './Hero.module.css'
+import { Container } from '../../styles'
+import { Formulario, FormTitle } from './styles'
 
 const Hero = () => (
-  <form className={styles.form}>
-    <div className="container">
-      <h2 className={styles.heroTitle}>
+  <Formulario>
+    <Container>
+      <FormTitle>
         As melhores vagas para tecnologia, design e artes visuais.
-      </h2>
-    </div>
-  </form>
+      </FormTitle>
+    </Container>
+  </Formulario>
 )
 
 export default Hero
